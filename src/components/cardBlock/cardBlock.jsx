07e53a86ee82bb29714e0cede21cb923dad2ba78
geroi657 <gyvayastar@gmail.com>
2024-06-09 02:19:06 +0700

    Added cardBlock, card and footer. Yet to do css for 'em

--- src/components/cardBlock/cardBlock.jsx
+++ src/components/cardBlock/cardBlock.jsx
@@ -1,5 +1,17 @@
-export default function CardBlock() {
+import Heading from "../heading/heading";
+import Paragraph from "../paragraph/paragraph";
+import Card from "../card/card";
+
+export default function CardBlock({level, headingContent, paragraphContent}) {
     return (
-        <></>
+        <div className="cardBlock-wrapper">
+            <Heading level={ level } headingContent={ headingContent }></Heading>
+            <Paragraph paragraphContent={ paragraphContent }></Paragraph>
+            <div className="cardBlock">
+                <Card backgroundURL="" headingContent="" paragraphContent=""></Card>
+                <Card backgroundURL="" headingContent="" paragraphContent=""></Card>
+                <Card backgroundURL="" headingContent="" paragraphContent=""></Card>
+            </div>
+        </div>
     );
 }
