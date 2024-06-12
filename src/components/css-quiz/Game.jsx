22267e6e35e298803b5308f5f473cc0acafdf5e2
some_012 <someone200315@gmail.com>
2024-06-12 23:37:13 +0700

    Add intro

--- src/components/css-quiz/Game.jsx
+++ src/components/css-quiz/Game.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Question from './questions';
 import questions from './list_questions';
 import './game.css';
+import { Link } from 'react-router-dom';
+import Button from '../button/button';
+import Intro from './intro';
 
 const Game = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [shuffledQuestions, setShuffledQuestions] = useState([]);
+  const [showIntro, setShowIntro] = useState(true);
 
   useEffect(() => {
     shuffleQuestions();
@@ -23,24 +27,28 @@ const Game = () => {
       setScore(score + 1);
     }
 
-
     const nextQuestionIndex = currentQuestionIndex + 1;
-    if (selectedOption != shuffledQuestions[currentQuestionIndex].answer) {
+    if (selectedOption !== shuffledQuestions[currentQuestionIndex].answer) {
       setCurrentQuestionIndex(currentQuestionIndex);
-    }
-    else if (nextQuestionIndex < shuffledQuestions.length) {
+    } else if (nextQuestionIndex < shuffledQuestions.length) {
       setCurrentQuestionIndex(nextQuestionIndex);
-    }
-    else {
+    } else {
       setShowScore(true);
     }
   };
 
+  const startGame = () => {
+    setShowIntro(false);
+  };
+
   return (
     <div className="game">
-      {showScore ? (
+      {showIntro ? (
+        <Intro startGame={startGame} />
+      ) : showScore ? (
         <div className="score-section">
-          <h2>Ваш результат: {score} из {shuffledQuestions.length}</h2>
+          <h2>Вы молодцы! Ваш результат: {score} из {shuffledQuestions.length}!</h2>
+          <Link to={"/"}><Button text="Выйти" /></Link>
         </div>
       ) : (
         shuffledQuestions.length > 0 && (
--- src/components/css-quiz/Questions.jsx
+++ src/components/css-quiz/Questions.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
-import './questions.css'
+import React, { useState, useEffect } from 'react';
+import './questions.css';
 
 const Question = ({ questionData, onAnswer }) => {
   const [selectedOption, setSelectedOption] = useState("");
   const [description, setDescription] = useState("");
+  const [shuffledOptions, setShuffledOptions] = useState([]);
+
+  useEffect(() => {
+    // Перемешиваем варианты ответа при каждом новом вопросе
+    setShuffledOptions(shuffleArray(questionData.options));
+  }, [questionData]);
+
+  const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  };
 
   const handleChange = (e) => {
     setSelectedOption(e.target.value);
@@ -13,14 +28,14 @@ const Question = ({ questionData, onAnswer }) => {
   const handleSubmit = () => {
     if (selectedOption) {
       onAnswer(selectedOption);
-      setDescription();
+      setDescription("");
     }
   };
 
   return (
     <div className="question">
       <h2>{questionData.question}</h2>
-      {questionData.options.map((option, index) => {
+      {shuffledOptions.map((option, index) => {
         const optionClass = (option === questionData.answer) ? 'correct' : 'incorrect';
         return (
           <div key={index} className={optionClass}>
